ef3a468ce51033a34443d84fac46af431449b18e
rflowturing <wolf.reip@gmail.com>
2020-10-15 14:21:12 +0200

    refactoring before cleaning code

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const days_temp = document.querySelectorAll(".days_temp");
 const hour = document.querySelectorAll(".hour");
 const day = document.querySelectorAll(".day");
 const day_temp_max = document.querySelectorAll(".days_temp_Max");
+const humidity = document.querySelector(".humidity");
+const vent = document.querySelector(".vent");
 
 //On récupère les données pour avoir les latitues, longitudes et le nom
 const getLatLongData = async (city = "Neerheylissem") => {
@@ -52,11 +54,17 @@ const getLatLong = (data) => {
 };
 
 const updateUiData = (data) => {
+  let deg = degToCard(data.current.wind_deg);
+  vent.innerHTML = `Vent : ${data.current.wind_speed} km/h ${deg}`;
+
+  humidity.innerHTML = `<span> Humidité : ${data.current.humidity}% </span>`;
   let descrip = data.current.weather[0].description;
   descrip = descrip.charAt(0).toUpperCase() + descrip.slice(1);
   temp.innerHTML = Math.trunc(data.current.temp) + "<span>&#8451</span>";
   description.innerHTML = descrip;
-  temp_ressentie.innerHTML = Math.trunc(data.current.feels_like);
+  temp_ressentie.innerHTML = `Temp ressentie : ${Math.trunc(
+    data.current.feels_like
+  )}&#8451`;
   img.src = `img/${data.current.weather[0].icon}.svg`;
 
   for (i = 0; i < 8; i++) {
@@ -78,7 +86,7 @@ const updateUiData = (data) => {
 const updateName = (data) => {
   loc.innerHTML = data.city.name;
   for (i = 1; i < 9; i++) {
-    hour[i - 1].innerHTML = `${data.list[i].dt_txt.slice(10, 13)}h`;
+    // hour[i - 1].innerHTML = `${data.list[i].dt_txt.slice(10, 13)}h`;
     // hour[i - 1].innerHTML = `${data.list[i].dt_txt.slice(10, 13)}h`;
   }
 };
@@ -126,3 +134,39 @@ let semaineOrdre = semaine
   .concat(semaine.slice(0, semaine.indexOf(jourActuelle)));
 
 console.log(semaineOrdre);
+
+var degToCard = function (deg) {
+  if (deg > 11.25 && deg < 33.75) {
+    return "NNE";
+  } else if (deg > 33.75 && deg < 56.25) {
+    return "ENE";
+  } else if (deg > 56.25 && deg < 78.75) {
+    return "E";
+  } else if (deg > 78.75 && deg < 101.25) {
+    return "ESE";
+  } else if (deg > 101.25 && deg < 123.75) {
+    return "ESE";
+  } else if (deg > 123.75 && deg < 146.25) {
+    return "SE";
+  } else if (deg > 146.25 && deg < 168.75) {
+    return "SSE";
+  } else if (deg > 168.75 && deg < 191.25) {
+    return "S";
+  } else if (deg > 191.25 && deg < 213.75) {
+    return "SSW";
+  } else if (deg > 213.75 && deg < 236.25) {
+    return "SW";
+  } else if (deg > 236.25 && deg < 258.75) {
+    return "WSW";
+  } else if (deg > 258.75 && deg < 281.25) {
+    return "W";
+  } else if (deg > 281.25 && deg < 303.75) {
+    return "WNW";
+  } else if (deg > 303.75 && deg < 326.25) {
+    return "NW";
+  } else if (deg > 326.25 && deg < 348.75) {
+    return "NNW";
+  } else {
+    return "N";
+  }
+};
